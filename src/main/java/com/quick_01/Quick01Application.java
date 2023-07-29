7dd196a1b4d09971c56628197966fe7cb93920f5
Sun Zifan <sunzifan@outlook.com>
2023-07-29 12:49:18 +0900

    initia..

---
+++ src/main/java/com/quick_01/Quick01Application.java
@@ -0,0 +1,13 @@
+package com.quick_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quick01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Quick01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/quick_01/Quick01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quick_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Quick01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
